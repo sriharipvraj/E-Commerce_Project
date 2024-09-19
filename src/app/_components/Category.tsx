76fee6b0c5409968414879348ead1cf7538f2447
sriharipvraj <sriharipvrj@gmail.com>
2024-09-19 10:35:48 +0530

    baseUrl

--- src/app/_components/Category.tsx
+++ src/app/_components/Category.tsx
@@ -5,64 +5,54 @@ import kidsImg from "../../../public/_categoryimages/kids.jpg";
 import accessoriesImg from "../../../public/_categoryimages/accessories.jpg";
 import CategoryCard from "./CategoryCard";
 import Link from "next/link";
+import { frontendApi } from "../api/fontendApi";
+import { storageUrl } from "@/utils/baseurl";
 
-const Category = () => {
-  const categoryList = [
-    {
-      image: menImg,
-      categoryName: "men",
-    },
-    {
-      image: womenImg,
-      categoryName: "women",
-    },
-    {
-      image: accessoriesImg,
-      categoryName: "accessories",
-    },
-    {
-      image: kidsImg,
-      categoryName: "kids",
-    },
-  ];
+const Category = async () => {
+  const response = await frontendApi.HomeCategory();
+  // console.log("::::::::::",response.data.data.categoryData);
+  const homeCategory = response.data.data.categoryData;
 
   return (
     <div className="md:p-10 flex flex-col items-center ">
       <div>
-        <h1 className="text-global-font-h6 md:text-global-font-h1 font-extrabold text-cyan-900" >Categories</h1>
+        <h1 className="text-global-font-h6 md:text-global-font-h1 font-extrabold text-cyan-900">
+          Categories
+        </h1>
       </div>
       <div>
-        <p className="text-cyan-500 font-semibold md:font-bold p-3" >Shop for your whole family</p>
+        <p className="text-cyan-500 font-semibold md:font-bold p-3">
+          Shop for your whole family
+        </p>
       </div>
       <div className="grid md:grid-cols-3 w-3/4 gap-5 md:h-3/4 h-1/2 grid-cols-1">
-        <div className="" >
-         
-            <CategoryCard
-              src={categoryList[0].image}
-              name={categoryList[0].categoryName}
-            />
-         
+        <div className="">
+          <CategoryCard
+            src={storageUrl + homeCategory[0].categoryImage}
+            name={homeCategory[0].categoryName}
+          />
         </div>
         <div>
           <CategoryCard
-            src={categoryList[1].image}
-            name={categoryList[1].categoryName}
+            src={storageUrl + homeCategory[1].categoryImage}
+            name={homeCategory[1].categoryName}
           />
         </div>
         <div className="flex flex-col md:gap-0.5 gap-5 ">
-            <div ><CategoryCard
-            src={categoryList[2].image}
-            name={categoryList[2].categoryName}
-            className={"md:h-48"}
-          /></div>
-          <div >
-          <CategoryCard
-            src={categoryList[3].image}
-            name={categoryList[3].categoryName}
-            className={"md:h-48"}
-          />
+          <div>
+            <CategoryCard
+              src={storageUrl + homeCategory[2].categoryImage}
+              name={homeCategory[2].categoryName}
+              className={"md:h-48"}
+            />
           </div>
-          
+          <div>
+            <CategoryCard
+              src={storageUrl + homeCategory[3].categoryImage}
+              name={homeCategory[3].categoryName}
+              className={"md:h-48"}
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/app/api/fontendApi.ts
+++ src/app/api/fontendApi.ts
@@ -3,25 +3,13 @@ import axios from "axios";
 export const frontendApi = {
   HomeCategory: async function () {
     return await axios.get(
-      "http://localhost:4000/api/dashboard/category/home/category"
+      "http://localhost:4000/api/frontend/homepage/category"
     );
   },
   HomeFeaturedProducts: async function () {
     return await axios.get(
-      "http://localhost:4000/api/dashboard/product/products/featured"
+      "http://localhost:4000/api/frontend/homepage/product/featured"
     );
   },
-  GetAllCategories: async function () {
-    return await axios.get(
-      "http://localhost:4000/api/dashboard/category/getcategories"
-    );
-  },
-  GetAllBrands: async function () {
-    return await axios.get(
-      "http://localhost:4000/api/dashboard/brands/getbrands"
-    );
-  },
-  GetProductsByCategory: async function (){
-    return await axios.get('');
-  }
+  
 };
